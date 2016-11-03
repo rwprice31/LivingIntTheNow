ecd6c4408329461ba5e58ca17e7d2c9670a6ee54
Connor W <mwilkinson@ggc.edu>
2016-11-03 14:24:59 +0000

    Ran db:migrate/reset

--- db/migrate/20161027220710_create_join_table_position_user.rb
+++ db/migrate/20161027220710_create_join_table_position_user.rb
@@ -1,8 +1,8 @@
 class CreateJoinTablePositionUser < ActiveRecord::Migration[5.0]
   def change
     create_join_table :positions, :users do |t|
-      t.integer :position_id
-      t.integer :user_id
+       #t.index [:position_id, :user_id]
+       #t.index [:user_id, :position_id]
     end
     add_index :positions_users, :user_id
     add_index :positions_users, :position_id
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027201958) do
+ActiveRecord::Schema.define(version: 20161027220710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,7 +96,6 @@ ActiveRecord::Schema.define(version: 20161027201958) do
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["store_id"], name: "index_users_on_store_id", using: :btree
-
   end
 
   add_foreign_key "locations", "stores"
