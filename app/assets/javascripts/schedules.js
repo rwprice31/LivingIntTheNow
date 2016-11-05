772b2d747fc3b7bd22a0f01f41d29fffd75462c3
jpsprouse <jsprouse@ggc.edu>
2016-11-05 02:42:16 +0000

    Modal receives dates

--- app/assets/javascripts/schedules.js
+++ app/assets/javascripts/schedules.js
@@ -4,12 +4,12 @@
 $(function() {
 $('#myModal').on('shown.bs.modal', function(e) {
 
+    var date = e.relatedTarget.dataset.date; 
     var userId = e.relatedTarget.dataset.user;
-    var date = e.relatedTarget.dataset.date;
     var position = e.relatedTarget.dataset.position;
-//get Start Time & if set format for Time standard
-    var start = e.relatedTarget.dataset.start;
 
+//get Start Time & if set format for Time standard
+var start = e.relatedTarget.dataset.start;
         if(typeof start != 'undefined'){
             start = start.replace( /[^0-9:]/g, '' );
             if(start.length < 5){
@@ -18,7 +18,7 @@ $('#myModal').on('shown.bs.modal', function(e) {
         };
 
 //get End Time & if set format for Time standard
-    var end = e.relatedTarget.dataset.end;
+var end = e.relatedTarget.dataset.end;
         if(typeof end != 'undefined'){
             end = end.replace( /[^0-9:]/g, '' );
             if(end.length < 5){
@@ -26,19 +26,26 @@ $('#myModal').on('shown.bs.modal', function(e) {
             };
         };
         
+    
+
 //temp data for development 
    // document.getElementById("devInfo").innerHTML = userId + " | " + end;
+   
+   
 //set form pre-data
-
     var startPrint = document.getElementById("schedule_startTime_4i").value;
-     document.getElementById("schedule_startTime_4i").value = start.substring(0, 2);
-     document.getElementById("schedule_startTime_5i").value = start.substring(3, 5);
-     document.getElementById("schedule_endTime_4i").value = end.substring(0,2);
-     document.getElementById("schedule_endTime_5i").value = end.substring(3,5);
-    // document.getElementById("date").value = date;
+     document.getElementById("schedule_startTime_4i").value = start.substring(0, 2);    //hour
+     document.getElementById("schedule_startTime_5i").value = start.substring(3, 5);    //minutes
+     document.getElementById("schedule_endTime_4i").value = end.substring(0,2);     //hour
+     document.getElementById("schedule_endTime_5i").value = end.substring(3,5);     //minutes
+     document.getElementById("schedule_date_1i").value = date.substring(0,4);      //year
+     document.getElementById("schedule_date_2i").value = date.substring(5,7);      //month
+     document.getElementById("schedule_date_3i").value = date.substring(8,10);      //day
      document.getElementById("schedule_user_id").value = userId;
      document.getElementById("schedule_position_id").value = position;
   
+  
+
 });
 
 
@@ -51,7 +58,7 @@ getFormattedTime = function (fourDigitTime) {
     var amPm = hours24 > 11 ? 'p' : 'a';
     var minutes = fourDigitTime.substring(2);
 
-    return hours + minutes + amPm;
+    return hours + minutes;
 };
 /* find all spans and replace their content*/
 $('span.mil_time').html(function( i, oldHtml){
