cb00bd6cb86f8dd4903bcc9c6aa2a9e462910ae3
Connor W <mwilkinson@ggc.edu>
2016-11-07 22:30:02 +0000

    Gemfile and stuff

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -65,7 +65,7 @@ class SchedulesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_schedule
-      @schedule = Schedule.find(params[:id])
+      @schedule = Schedule.new
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
