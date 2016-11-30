4f300908b15c14c8eb8faa81db7e0425ab9f39a5
Kenny Cauthen <kenjacau@gmail.com>
2016-11-30 12:06:16 -0500

    indexing positions now shows only positions related to the admins related store id, Fixed some merging issues, fixed authentication issues, fixed welcome page.

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -4,7 +4,7 @@ class PositionsController < ApplicationController
   # GET /positions
   # GET /positions.json
   def index
-    @positions = Position.all
+    @positions = Position.where(store: current_user.store_id)
   end
 
   # GET /positions/1
